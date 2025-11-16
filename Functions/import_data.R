31c98d5b4f3529018074efb9dd77a7d670032b80
William Alexander Aguirre Antolínez <86641219+AguirreAntolinez@users.noreply.github.com>
2025-11-15 19:33:35 -0500

    Add files via upload

---
+++ Functions/import_data.R
@@ -0,0 +1,59 @@
+#Import data function
+#Author: William Alexander Aguirre Antolínez
+#Date: 15-11-2025
+
+#Description: This function imports data from this repository.
+
+#Arguments
+#name_list: character string. 
+#name_directory: character string.
+#name_years: vector.
+#names_months: vector.
+#root: character string.
+
+#See Processing scripts for examples of use.
+
+
+library(readr)
+
+import_data <- function(name_list,
+                        name_directory,
+                        names_years,
+                        names_months,
+                        root) {
+  
+  #Create the list 
+  create_list<-list()
+  
+  #For in years
+  for (y in names_years) {
+    data_year<- list()
+    #For in months
+    for (m in names_months) {
+      tryCatch(
+        df_year <- read_delim(
+          paste0(name_directory,y,"/",m,"/",root),
+          delim = ";",
+          escape_double = FALSE,
+          trim_ws = TRUE
+        ),
+        error = function(e) {
+          message(paste("Can´t was imported:", y, m, "->", e$message))
+          return(NULL)
+        }
+      )
+      data_year[[m]]<-df_year
+      df_year<-NULL
+      rm(df_year)
+    }
+    create_list[[y]]<-data_year
+    data_year<-NULL
+    rm(data_year)
+    
+  }
+  
+  #Rename de list
+  assign(name_list, create_list, envir = .GlobalEnv)
+  
+}
+
